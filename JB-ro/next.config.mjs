430a99acf81585c18ae6922fdfcc033b1c69be01
DESKTOP-8KD149H\user1 <taegyeong0519@gmail.com>
2026-05-14 11:53:28 +0900

    인기 여행지 목록 조회 연결

--- JB-ro/next.config.mjs
+++ JB-ro/next.config.mjs
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  allowedDevOrigins: ['192.168.10.28', '192.168.10.29'],
+  allowedDevOrigins: [
+    '192.168.10.28',
+    '192.168.10.29',
+  ],
+
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:8081/api/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
